a986ecbe1e56308fb46e938d260dc9fafee07ed8
n1k35h <119499198+n1k35h@users.noreply.github.com>
2023-05-16 21:50:54 +0100

    fixed the bugs to connect to the database
    added the print('Table Uploaded') to check if the table is added to the database

--- database_utils.py
+++ database_utils.py
@@ -1,7 +1,7 @@
 import yaml
 from sqlalchemy import create_engine
 from sqlalchemy import inspect
-import pandas as pd
+# import pandas as pd
 
 
 # class DatabaseConnector will use to connect with and upload data to the database
@@ -12,8 +12,8 @@ class DatabaseConnector:
     def read_db_creds(self):
         # read the credentials yaml file and returns a dictionary of the credentials
         with open('db_creds.yaml', 'r') as f:
-            data = yaml.safe_load(f)
-        return data
+            creds_data = yaml.safe_load(f)
+        return creds_data
 
         # print(data)
 
@@ -23,16 +23,17 @@ class DatabaseConnector:
         # return an sqlalchemy database engine
         creds_data = self.read_db_creds() # reads the credentials
         engine = create_engine(f"{'postgresql'}+{'psycopg2'}://{creds_data['RDS_USER']}:{creds_data['RDS_PASSWORD']}@{creds_data['RDS_HOST']}:{creds_data['RDS_PORT']}/{creds_data['RDS_DATABASE']}")
+        engine.connect()
         return engine
 
     def list_db_tables(self):
         # this method will list all tables in the database so that the data can be extract from the relevent tables
-        eng = self.init_db_engine()
-        inspector = inspect(eng)
-        table_names = inspector.get_table_names()
-        return table_names
+        engine = self.init_db_engine()
+        inspector = inspect(engine)
+        table_name = inspector.get_table_names()
+        return table_name
 
-    def upload_to_db(self, df, table_name):
+    def upload_to_db(self, df, table_name,):
         # takes pandas DataFrame and table name to upload to as an argument
         # use the upload_to_db method to store the data in your sales_data database
         # in a table named dim_users
@@ -40,10 +41,13 @@ class DatabaseConnector:
         DBAPI = 'psycopg2'
         HOST = 'localhost'
         USER = 'postgres'
-        PASSWORD = 'P4wP4tr0lCh453'
+        PASSWORD = '5c008yd00d00'
         PORT = 5432
         DATABASE = 'Sales_Data'
 
-        engine = create_engine(f"{DATABASE_TYPE}+{DBAPI}://{USER}:{PASSWORD}@{HOST}:{PORT}/{DATABASE}")
-        engine.connect()
-        df.to_sql(name=table_name, con=engine, if_exists='replace')
+        localengine = create_engine(f"{DATABASE_TYPE}+{DBAPI}://{USER}:{PASSWORD}@{HOST}:{PORT}/{DATABASE}")
+        localengine.connect()
+        df.to_sql(name=table_name, con=localengine, if_exists='replace')
+        print('Table Uploaded')
+
+conn = DatabaseConnector()

5daccfd9f279052213b190d2b417e483b0c0dec6
n1k35h <119499198+n1k35h@users.noreply.github.com>
2023-06-21 21:45:24 +0100

    Updated M3_T4.sql

--- sql_task/M3_T4.sql
+++ sql_task/M3_T4.sql
@@ -4,13 +4,16 @@ New column is created to allow the team to make quick decision on delivery weigh
 UPDATE dim_products
 SET product_price = REPLACE(product_price, '£', '');
 
-SELECT *,
-(CASE
+ALTER TABLE dim_products
+	ADD COLUMN weight_class VARCHAR(255);
+
+UPDATE dim_products
+SET weight_class = 
+	CASE
 	WHEN weight < 2.0 THEN 'Light'
 	WHEN weight >= 2.0 AND weight < 40.0 THEN 'Mid_Sized'
 	WHEN weight >= 40.0 AND weight < 140.0 THEN 'Heavy'
 	WHEN weight >= 140.0 THEN 'Truck_Required'
-END) AS weight_class
-FROM dim_products
-ORDER BY index asc;
+	END;
 
+SELECT * FROM dim_products ORDER BY index ASC;

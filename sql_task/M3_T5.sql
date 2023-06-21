ac032db103e4c08d5324ebeac29bc4cc1447f39e
n1k35h <119499198+n1k35h@users.noreply.github.com>
2023-06-21 22:24:20 +0100

    Updated M3_T5.sql

--- sql_task/M3_T5.sql
+++ sql_task/M3_T5.sql
@@ -12,14 +12,8 @@ ALTER TABLE dim_products
 	ALTER COLUMN "EAN" TYPE VARCHAR(18),
 	ALTER COLUMN product_code TYPE VARCHAR(11),
 	ALTER COLUMN date_added TYPE DATE,
-	ALTER COLUMN uuid TYPE UUID USING CAST(uuid as UUID);
--- 	ALTER COLUMN still_available TYPE BOOL USING still_available::boolean;
+	ALTER COLUMN uuid TYPE UUID USING CAST(uuid as UUID),
+	ALTER COLUMN still_available TYPE BOOL USING (still_available = 'Still_available'),
+    	ALTER COLUMN weight_class TYPE VARCHAR(14);
 
-SELECT *,
-	CASE
-		WHEN weight < 2.0 THEN 'Light'
-		WHEN weight >= 2.0 AND weight < 40.0 THEN 'Mid_Sized'
-		WHEN weight >= 40.0 AND weight < 140.0 THEN 'Heavy'
-		WHEN weight >= 140.0 THEN 'Truck_Required'
-	END AS weight_class
-FROM dim_products ORDER BY index asc;
+SELECT * FROM dim_products ORDER BY index asc;

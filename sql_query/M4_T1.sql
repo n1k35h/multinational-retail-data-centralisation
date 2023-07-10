417b6e004f4091f10c43f8920fecbfa421d57dc9
n1k35h <119499198+n1k35h@users.noreply.github.com>
2023-07-10 22:56:28 +0100

    Delete sql_query directory

--- sql_query/M4_T1.sql
+++
@@ -1,9 +0,0 @@
-/* The below query will give the Operations team the information on which countries that are currently
-operating and which country has the most stores. */
-
-SELECT country_code AS country,
-COUNT(store_code) AS total_no_stores
-FROM dim_store_details
-GROUP BY country
-ORDER BY total_no_stores DESC limit 3;
-
--- sql_query/M4_T7.sql
+++
@@ -1,7 +0,0 @@
-/* The below query gives data on overall head count in each country */
-
-SELECT SUM(staff_numbers) AS total_staff_numbers,
-	country_code
-	FROM dim_store_details
-	GROUP BY country_code
-	ORDER BY total_staff_numbers DESC;

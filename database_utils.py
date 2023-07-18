ad9b46a5e91add8a6a768e5cf820a6b8169030ff
n1k35h <119499198+n1k35h@users.noreply.github.com>
2023-07-18 11:57:58 +0100

    docstring is added

--- database_utils.py
+++ database_utils.py
@@ -3,18 +3,18 @@ from sqlalchemy import create_engine
 from sqlalchemy import inspect
 # import pandas as pd
 
-# class DatabaseConnector will use to connect with and upload data to the database
+# class DatabaseConnector will use to connect and upload tables to the database
 class DatabaseConnector:
 
     def read_db_creds(self, file):
-        # read the credentials yaml file and returns a dictionary of the credentials
+        """ reads the credentials yaml file and returns a dictionary of the credentials """
         with open(file, 'r') as f:
             creds_data = yaml.safe_load(f)
         return creds_data
 
     def init_db_engine(self):
-        # this method will read the credentials from the return of read_db_creds method and initialise and
-        # return an sqlalchemy database engine
+        """ this method will read the credentials from the return of read_db_creds method and initialise and
+        return an sqlalchemy database engine """
         creds_data = self.read_db_creds('db_creds.yaml') # reads the credentials
         DATABASE_TYPE = 'postgresql'
         DBAPI = 'psycopg2'
@@ -28,16 +28,16 @@ class DatabaseConnector:
         return engine
 
     def list_db_tables(self):
-        # this method will list all tables in the database so that the data can be extract from the relevent tables
+        """ this method will list all tables in the database so that the data can be extract from the relevent tables """
         engine = self.init_db_engine()
         inspector = inspect(engine)
         table_name = inspector.get_table_names()
         return table_name
 
     def upload_to_db(self, df, table_name,):
-        # takes pandas DataFrame and table name to upload to as an argument
-        # use the upload_to_db method to store the data in your sales_data database
-        # in a table named dim_users
+        """ takes pandas DataFrame and table name to upload to as an argument
+        use the upload_to_db method to store the data in your sales_data database
+        in a table named dim_users """
         creds_data = self.read_db_creds('local_creds.yaml')
         DATABASE_TYPE = 'postgresql'
         DBAPI = 'psycopg2'
